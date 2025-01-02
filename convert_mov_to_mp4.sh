3ebc18f566e1a1b0f8114c24604ad65b5dcd2031
Jack Nugent <jacknugent@MacBookPro.hsd1.md.comcast.net>
2025-01-02 17:01:40 -0500

    Add broken metronome ideo

--- convert_mov_to_mp4.sh
+++ convert_mov_to_mp4.sh
@@ -9,7 +9,7 @@ for file in *.mov; do
   
   # Convert .mov to .mp4 using FFmpeg with settings for maximum compatibility
   # Includes downscaling bit depth to 8 bits
-  ffmpeg -i "$file" -vf "scale=720:1280" -pix_fmt yuv420p -vcodec libx264 -profile:v baseline -level 3.0 -acodec aac -b:a 128k "$newfile"
+  ffmpeg -i "$file" -vf "scale=1280:720" -pix_fmt yuv420p -vcodec libx264 -profile:v baseline -level 3.0 -acodec aac -b:a 128k "$newfile"
   
   # Check if the conversion was successful
   if [ $? -eq 0 ]; then
